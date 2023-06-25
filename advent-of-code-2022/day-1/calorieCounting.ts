392325f0a39d77a0ba495822e2b1afb900be26a1
Felipe <falmeidagomes13@gmail.com>
2023-06-25 20:23:33 -0300

    feat: first day complete

---
+++ advent-of-code-2022/day-1/calorieCounting.ts
@@ -0,0 +1,51 @@
+type ElfWithMostCaloriesProps = {
+	elf: number;
+	calories: null | number;
+};
+
+const calories = `1000
+2000
+3000
+
+4000
+7000
+10000
+10000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`;
+
+const findElfWithMostCalories = (calories: string) => {
+	const arrayCalories = calories.split('\n');
+	let elf = 1;
+	let elfWithMostCalories: ElfWithMostCaloriesProps = { elf: 1, calories: null };
+	let someOfCalories = 0;
+
+	for (let i = 0; i < arrayCalories.length; i++) {
+		if (arrayCalories[i] === '') {
+			if (
+				elfWithMostCalories.calories === null ||
+				elfWithMostCalories.calories < someOfCalories
+			) {
+				elfWithMostCalories.elf = elf;
+				elfWithMostCalories.calories = someOfCalories;
+			}
+			elf++;
+			someOfCalories = 0;
+		} else {
+			someOfCalories += +arrayCalories[i];
+		}
+	}
+	return { ...elfWithMostCalories };
+};
+
+const elfWithMostCalories = findElfWithMostCalories(calories);
+console.log(
+	`The Elf with most calories is ${elfWithMostCalories.elf}° elf, he has ${elfWithMostCalories.calories} calories`,
+);
